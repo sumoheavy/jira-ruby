fc1873bab3e3643981e47dfad5a3ae136e1cd8f6
Malcolm Locke <malc@wholemeal.co.nz>
2011-12-12 20:48:44 +1300

    Allow passing access token details to example.rb

--- example.rb
+++ example.rb
@@ -9,14 +9,25 @@ CONSUMER_KEY = 'test'
 
 client = Jira::Client.new(CONSUMER_KEY, '', options)
 
-request_token = client.request_token
-puts "Opening #{request_token.authorize_url}"
-system "open #{request_token.authorize_url}"
+if ARGV.length == 0
+  # If not passed any command line arguments, open a browser and prompt the
+  # user for the OAuth verifier.
+  request_token = client.request_token
+  puts "Opening #{request_token.authorize_url}"
+  system "open #{request_token.authorize_url}"
 
-puts "Enter the oauth_verifier: "
-oauth_verifier = gets.strip
+  puts "Enter the oauth_verifier: "
+  oauth_verifier = gets.strip
 
-client.init_access_token(:oauth_verifier => oauth_verifier)
+  access_token = client.init_access_token(:oauth_verifier => oauth_verifier)
+  puts "Access token: #{access_token.token} secret: #{access_token.secret}"
+elsif ARGV.length == 2
+  # Otherwise assume the arguments are a previous access token and secret.
+  access_token = client.set_access_token(ARGV[0], ARGV[1])
+else
+  # Script must be passed 0 or 2 arguments
+  raise "Usage: #{$0} [ token secret ]"
+end
 
 # Show all projects
 projects = client.Project.all
