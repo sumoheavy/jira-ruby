82143c72175ad346211a4d8d27a3563902eb4e6d
Greg Signal <greg.signal@trineo.co.nz>
2011-12-15 10:27:26 +1300

    Add component PUT and POST integration tests

--- spec/integration/component_spec.rb
+++ spec/integration/component_spec.rb
@@ -24,6 +24,12 @@ describe Jira::Resource::Component do
     stub_request(:delete,
                  "http://localhost:2990/jira/rest/api/2/component/10000").
                  to_return(:body => nil)
+    stub_request(:post,
+                 "http://localhost:2990/jira/rest/api/2/component").
+                 to_return(:status => 201, :body => get_mock_response('component.post.json'))
+    stub_request(:put,
+                 "http://localhost:2990/jira/rest/api/2/component/10000").
+                 to_return(:status => 200, :body => get_mock_response('component/10000.put.json'))
   end
 
   it "should get a single component by id" do
@@ -42,6 +48,22 @@ describe Jira::Resource::Component do
   it "deletes a component" do
     component = client.Component.build('id' => "10000")
     component.delete.should be_true
+  end
+
+  it "saves a new component" do
+    component = client.Component.build({"name" => "Test component", "project" => "SAMPLEPROJECT"})
+    component.save.should be_true
+    component.id.should   == "10001"
+    component.name.should == "Test component"
+  end
+
+  it "saves an existing component" do
+    component = client.Component.build('id' => '10000')
+    component.fetch
+    component.attrs['name'] = "Jammy"
+    component.save.should be_true
+    component.id.should == "10000"
+    component.name.should == "Jammy"
   end
 
 end
