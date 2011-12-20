35faedd52209b8709d697695c323560da69622da
wasabhi <abhinav.keswani@trineo.co.nz>
2011-12-20 13:27:23 +1100

    Clean up original ineffective rake task loading

--- lib/jira.rb
+++ lib/jira.rb
@@ -1,5 +1,3 @@
-Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f } if defined?(Rake)
-
 $: << File.expand_path(File.dirname(__FILE__))
 require 'jira/resource/base'
 require 'jira/resource/base_factory'
