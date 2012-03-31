02d0ee320cbd16ce29fa837cb3f6a1d146b28cc8
Paul Morton <pmorton@biaprotect.com>
2012-03-31 09:23:48 -0700

    Add Support for changelogs

--- lib/jira/resource/issue.rb
+++ lib/jira/resource/issue.rb
@@ -29,12 +29,48 @@ module JIRA
 
       has_many :worklogs, :nested_under => ['fields','worklog']
 
+<<<<<<< HEAD
       def self.all(client)
         response = client.get(client.options[:rest_base_path] + "/search")
         json = parse_json(response.body)
         json['issues'].map do |issue|
           client.Issue.build(issue)
         end
+=======
+      def self.all(client, jql = nil)
+        issues = []
+        fetched_results = 0
+        begin 
+          url = client.options[:rest_base_path] + "/search?startAt=#{fetched_results}"
+          url << "&jql=#{ URI.escape(jql) }" if jql
+          url << "&expand=all"
+
+          response = client.get(url)
+          json = parse_json(response.body)
+          
+          issues = issues + json['issues'].map do |issue|
+            client.Issue.build(issue)
+          end
+
+          fetched_results += json['maxResults']
+
+        end while fetched_results < json['total']
+
+        issues
+>>>>>>> 44fa30a... Add support for change logs
+      end
+
+      def changelogs
+        change_histories = []
+
+        url = client.options[:rest_base_path] + "/issue/#{id}?expand=changelog"
+        response = client.get(url)
+        json = self.class.parse_json(response.body)
+        change_histories = json['changelog']['histories'].map do |h|
+          client.Changelog.build(h)
+        end
+        change_histories
+
       end
 
       def respond_to?(method_name)
