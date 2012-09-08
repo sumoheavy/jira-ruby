e5ecde80c96003fcbde296acc6c7b013154c1c26
Omachonu Ogali <oogali@gmail.com>
2012-09-08 10:39:35 -0400

    mark this as an upload, send attachment attributes raw, expand the result

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -12,6 +12,15 @@ module JIRA
       def self.collection_path(client, prefix = '/')
         client.options[:rest_base_path] + prefix + self.endpoint_name + 's'
       end
+
+      def save!(attrs)
+        http_method = :upload
+        response = client.send(http_method, url, attrs)
+        set_attrs(attrs, false)
+        set_attrs_from_response(response)
+        @expanded = true
+        true
+      end
     end
 
   end
