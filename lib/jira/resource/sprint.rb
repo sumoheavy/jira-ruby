799ed851f4b375ba1837de74237e0632f3d40e84
Alex Stone <astone@zendesk.com>
2013-06-20 13:10:33 -0700

    Adds sprint to gem to test usage w/ client

---
+++ lib/jira/resource/sprint.rb
@@ -0,0 +1,21 @@
+module JIRA
+  module Resource
+
+    class SprintFactory < JIRA::BaseFactory # :nodoc:
+    end
+
+    class Sprint < JIRA::Base
+
+      has_many :completed_issues,   :nested_under => 'contents'
+      has_many :incompleted_issues, :nested_under => 'contents'
+      has_many :punted_issues,      :nested_under => 'contents'
+
+      def self.find(client, rapid_view_id, sprint_id, options = {})
+        instance = self.new(client, options)
+        instance.attrs[]
+        instance.fetch
+        instance 
+      end
+    end
+  end
+end
