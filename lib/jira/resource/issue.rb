e908bba7b805b9df29464e5f33d736d417b5db74
Jordan Harband <ljharb@gmail.com>
2014-01-08 18:36:47 -0800

    Make sure issue requests also get transition info.

--- lib/jira/resource/issue.rb
+++ lib/jira/resource/issue.rb
@@ -34,7 +34,10 @@ module JIRA
       has_many :worklogs, :nested_under => ['fields','worklog']
 
       def self.all(client)
-        response = client.get(client.options[:rest_base_path] + "/search")
+        response = client.get(
+          client.options[:rest_base_path] + "/search",
+          :expand => 'transitions.fields'
+        )
         json = parse_json(response.body)
         json['issues'].map do |issue|
           client.Issue.build(issue)
