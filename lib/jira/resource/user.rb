ea50b3ac54aebd73fd752cbb0e8da976f07fec18
Adam Bedford <abedford@mavenlink.com>
2015-03-27 16:23:35 -0700

    Adds a workaround way of getting all users

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -5,9 +5,20 @@ module JIRA
     end
 
     class User < JIRA::Base
+
       def self.singular_path(client, key, prefix = '/')
         collection_path(client, prefix) + '?username=' + key
       end
+
+      def self.all(client)
+        project_list = client.Project.all.map(&:key).join(',')
+        response = client.get("#{client.options[:rest_base_path]}/user/assignable/multiProjectSearch?projectKeys=#{project_list}")
+        users = parse_json(response.body)
+        users.map do |user|
+          client.User.build(user)
+        end
+      end
+
     end
 
   end
