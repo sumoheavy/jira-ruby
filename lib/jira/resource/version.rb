02431cc62395233d9ac6cfddffa89847a31304f5
Synchunk <synchunk@web.de>
2015-05-04 17:23:50 +0200

    Fix missing "project" option in resource/version.rb

--- lib/jira/resource/version.rb
+++ lib/jira/resource/version.rb
@@ -4,7 +4,29 @@ module JIRA
     class VersionFactory < JIRA::BaseFactory # :nodoc:
     end
 
-    class Version < JIRA::Base ; end
+    class Version < JIRA::Base
+      belongs_to :project
+
+      nested_collections true
+
+      def self.endpoint_name
+        'versions'
+      end
+
+      def self.all(client, options = {})
+        project = options[:project]
+        unless project
+          raise ArgumentError.new("project is required")
+        end
+
+        path = "#{project.self}/#{endpoint_name}"
+        response = client.get(path)
+        json = parse_json(response.body)
+        json.map do |version|
+          project.versions.build(version)
+        end
+      end
+    end
 
   end
 end
