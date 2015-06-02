1ca14317fad9836ce679e3941b44f605e9285311
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2015-06-02 09:59:11 +0300

    Adding some logging.

--- lib/jira/http_client.rb
+++ lib/jira/http_client.rb
@@ -25,7 +25,7 @@ module JIRA
       add_cookies(request) if options[:use_cookies]
       request.basic_auth(@options[:username], @options[:password])
       response = basic_auth_http_conn.request(request)
-      puts "<<<<<<<<<<<<<< Response body: #{response.inspect}"
+      puts "<<<<<<<<<<<<<< Response body: #{response.body}"
       store_cookies(response) if options[:use_cookies]
       response
     end
