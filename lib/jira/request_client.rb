0d6153d845c2b4aa13c41f3028fa2f08a7345218
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2015-06-07 14:08:49 +0300

    Adding debug.

--- lib/jira/request_client.rb
+++ lib/jira/request_client.rb
@@ -1,6 +1,7 @@
 require 'oauth'
 require 'json'
 require 'net/https'
+require 'pry'
 
 module JIRA
   class RequestClient
@@ -11,7 +12,7 @@ module JIRA
 
     def request(*args)
       response = make_request(*args)
-      binding.pry
+      binding.pry unless response.kind_of?(Net::HTTPSuccess)
       raise HTTPError.new(response) unless response.kind_of?(Net::HTTPSuccess)
       response
     end
