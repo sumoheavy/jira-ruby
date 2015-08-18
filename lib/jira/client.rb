6e0af49220f20aef794b0d154923cb4c12a498d7
Rodolfo Sikora <sikora@gmail.com>
2015-08-18 15:34:58 -0300

    trying to add sprint feature

--- lib/jira/client.rb
+++ lib/jira/client.rb
@@ -151,6 +151,9 @@ module JIRA
 
     def Remotelink
       JIRA::Resource::RemotelinkFactory.new(self)
+
+    def Sprint
+      JIRA::Resource::SprintFactory.new(self)
     end
 
     # HTTP methods without a body
