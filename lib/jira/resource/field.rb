e88b967e1b75d7de3607d0f4ac8d92aba7e59cfd
Paul Casto <pcasto@infoblox.com>
2016-03-06 18:40:37 -0500

    improved handling for customfield_##### fields

--- lib/jira/resource/field.rb
+++ lib/jira/resource/field.rb
@@ -6,23 +6,44 @@ module JIRA
     end
 
     class Field < JIRA::Base
+
+      #translate a custom field description to a method-safe name
+      def self.safe_name(description)
+        description.gsub(/[^a-zA-Z0-9]/,'_')
+      end
+
+      # safe_name plus disambiguation if it fails it uses the original jira id (customfield_#####)
+      def self.safer_name(description, jira_id)
+        "#{safe_name(description)}_#{jira_id.split('_')[1]}" rescue jira_id
+      end
+
       def self.map_fields(client)
         field_map = {}
         field_map_reverse = {}
         fields = client.Field.all
-        fields.each {|f|
+        # two pass approach, so that a custom field with the same name
+        # as a system field can't take precedence
+        fields.each do |f|
+          next if f.custom
+          name = safe_name(f.name)
+          field_map_reverse[f.id] = [f.name, name] # capture both the official name, and the mapped name
+          field_map[name] = f.id
+        end
+
+        fields.each do |f|
+          next unless f.custom
           name = if field_map.key? f.name
-            renamed = ("#{f.name}_#{f.id.split('_')[1]}").gsub(/[^a-zA-Z0-9]/,'_') rescue f.id
+            renamed = safer_name(f.name, f.id)
             warn "Duplicate Field name #{f.name} #{f.id} - renaming as #{renamed}"
             renamed
           else
-            f.name.gsub(/[^a-zA-Z0-9]/,'_')
+            safe_name(f.name)
           end
-          field_map[name] = f.id
           field_map_reverse[f.id] = [f.name, name] # capture both the official name, and the mapped name
-        }
-        client.cache.field_map = field_map
+          field_map[name] = f.id
+        end
         client.cache.field_map_reverse = field_map_reverse   # not sure where this will be used yet, but sure to be useful
+        client.cache.field_map = field_map
       end
 
       def self.field_map(client)
@@ -33,6 +54,32 @@ module JIRA
         field_name = field_name.to_s
         return field_name unless client.cache.field_map && client.cache.field_map[field_name]
         client.cache.field_map[field_name]
+      end
+
+      def respond_to?(method_name, include_all=false)
+        if [method_name.to_s, client.Field.name_to_id(method_name)].any? {|k| attrs.key?(k)}
+# "responds to either  #{method_name.to_s}  or #{client.Field.name_to_id(method_name)}"
+          true
+        else
+#warn "does parent respond to #{method_name}"
+          super(method_name)
+        end
+      end
+
+      def method_missing(method_name, *args, &block)
+          if attrs.keys.include?(method_name.to_s)
+#warn "responding to key #{method_name.to_s}"
+            attrs[method_name.to_s]
+          else
+            official_name=client.Field.name_to_id(method_name)
+#warn "responding to #{method_name} with official #{official_name}"
+            if attrs.keys.include?(official_name)
+              attrs[official_name]
+            else
+#warn "passing to parent to respond to #{method_name}"
+              super(method_name, *args, &block)
+            end
+          end
       end
     end
   end
--- spec/jira/resource/field_spec.rb
+++ spec/jira/resource/field_spec.rb
@@ -2,23 +2,131 @@ require 'spec_helper'
 
 describe JIRA::Resource::Field do
 
+  let(:cache) { OpenStruct.new }
+
   let(:client) do
     client = double(options: {rest_base_path: '/jira/rest/api/2'}  )
-    allow(client).to receive(:Field).and_return(JIRA::Resource::FieldFactory.new(client))
-    allow(client).to receive(:cache).and_return(OpenStruct.new)
+    field = JIRA::Resource::FieldFactory.new(client)
+    allow(client).to receive(:Field).and_return(field)
+    allow(client).to receive(:cache).and_return(cache)
+    # info about all fields on the client
+    allow(client.Field).to receive(:all).and_return([
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"customfield_10666", "name" => "Priority",   "custom" => true,  "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["cf[10666]","Priority"],         "schema" =>{"type" => "string",    "custom" => "com.atlassian.jira.plugin.system.customfieldtypes:select","customId" => 10666}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"issuekey",          "name" => "Key",        "custom" => false, "orderable" => false, "navigable" => true, "searchable" => false, "clauseNames" => ["id","issue","issuekey","key"]}),
+      JIRA::Resource::Field.new(client, :attrs => {"id" =>"priority",          "name" => "Priority",   "custom" => false, "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["priority"],                     "schema" =>{"type" => "priority",  "system" => "priority"}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"summary",           "name" => "Summary",    "custom" => false, "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["summary"],                      "schema" =>{"type" => "string",    "system" => "summary"}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"issuetype",         "name" => "Issue Type", "custom" => false, "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["issuetype","type"],             "schema" =>{"type" => "issuetype", "system" => "issuetype"}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"customfield_10111", "name" => "SingleWord", "custom" => true,  "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["cf[10111]","SingleWord"],       "schema" =>{"type" => "string",    "custom" => "com.atlassian.jira.plugin.system.customfieldtypes:select","customId" => 10111}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"customfield_10222", "name" => "Multi Word", "custom" => true,  "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["cf[10222]","Multi Word"],       "schema" =>{"type" => "string",    "custom" => "com.atlassian.jira.plugin.system.customfieldtypes:select","customId" => 10222}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"customfield_10333", "name" => "Why/N@t",    "custom" => true,  "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["cf[10333]","Why/N@t"],          "schema" =>{"type" => "string",    "custom" => "com.atlassian.jira.plugin.system.customfieldtypes:select","customId" => 10333}}),
+      JIRA::Resource::Field.new(client, :attrs => {'id' =>"customfield_10444", "name" => "SingleWord", "custom" => true,  "orderable" => true,  "navigable" => true, "searchable" => true,  "clauseNames" => ["cf[10444]","SingleWord"],       "schema" =>{"type" => "string",    "custom" => "com.atlassian.jira.plugin.system.customfieldtypes:select","customId" => 10444}})
+    ])
     client
   end
 
   describe "field_mappings" do
-    subject {
-      JIRA::Resource::Field.new(client, :attrs => {
-        'priority' => 1
-      })
-    }
 
-    it "can find a standard field" do
-      expect(subject.priority).to eq(1)
+    shared_context "mapped or not" do
+
+      subject {
+        JIRA::Resource::Field.new(client, :attrs => {
+          'priority' => 1,
+          'customfield_10111' => 'data_in_custom_field',
+          'customfield_10222' => 'multi word custom name',
+          'customfield_10333' => 'complex custom name',
+          'customfield_10444' => 'duplicated custom name',
+          'customfield_10666' => 'duplicate of a system name',
+        })
+      }
+
+      it "can find a standard field by id" do
+        expect(subject.priority).to eq(1)
+      end
+
+      it "can find a custom field by customfield_##### name" do
+        expect(subject.customfield_10111).to eq('data_in_custom_field')
+      end
+
+      it "is not confused by common attribute keys" do
+        expect{subject.name}.to raise_error(NoMethodError)
+        expect{subject.custom}.to raise_error(NoMethodError)
+        expect(subject.id).to eq(nil)   # picks up ID from the parent -
+      end
+    end
+
+    context "before fields are mapped" do
+
+      include_context "mapped or not"
+
+      it "can find a standard field by id" do
+        expect(subject.priority).to eq(1)
+      end
+
+      it "cannot find a standard field by name before mapping" do
+        expect{subject.Priority}.to raise_error(NoMethodError)
+      end
+
+      it "can find a custom field by customfield_##### name" do
+        expect(subject.customfield_10111).to eq('data_in_custom_field')
+      end
+
+      it "cannot find a mapped field before mapping and raises error" do
+        expect{subject.SingleWork}.to raise_error(NoMethodError)
+      end
+
+      it "is not confused by common attribute keys and raises error" do
+        expect{subject.name}.to raise_error(NoMethodError)
+        expect{subject.custom}.to raise_error(NoMethodError)
+        expect(subject.id).to eq(nil)   # picks up ID from the parent -
+      end
+    end
+
+    context "after fields are mapped" do
+
+      before do
+        silence_stream(STDERR) do
+          expect(client.Field.map_fields.class).to eq(Hash)
+        end
+      end
+
+      include_context "mapped or not"
+
+      it "warns of duplicate fields" do
+        expect{client.Field.map_fields}.to output(/renaming as Priority_10666/).to_stderr
+        expect{client.Field.map_fields}.to output(/renaming as SingleWord_10444/).to_stderr
+      end
+
+      it "can find a mapped field after mapping and returns results" do
+        expect{subject.SingleWord}.to_not raise_error
+        expect(subject.SingleWord).to eq subject.customfield_10111
+      end
+
+      it "handles duplicate names in a safe fashion" do
+        expect{subject.Multi_Word}.to_not raise_error
+        expect(subject.Multi_Word).to eq subject.customfield_10222
+      end
+
+      it "handles special characters in a safe fashion" do
+        expect{subject.Why_N_t}.to_not raise_error
+        expect(subject.Why_N_t).to eq subject.customfield_10333
+      end
+
+      it "handles duplicates in custom names" do
+        expect{subject.SingleWord_10444}.to_not raise_error
+        expect(subject.SingleWord_10444).to eq subject.customfield_10444
+      end
+
+      it "keeps custom names from overwriting system names" do
+        #expect(client.Field.map_fields.class).to eq(Hash)
+        expect{subject.Priority_10666}.to_not raise_error
+        expect(subject.Priority_10666).to eq subject.customfield_10666
+      end
+
+      it "can find a standard field by an expanded name" do
+        #expect(client.Field.map_fields.class).to eq(Hash)
+        expect(subject.priority).to eq(1)
+        expect(subject.Priority).to eq(1)
+      end
     end
   end
-
 end
