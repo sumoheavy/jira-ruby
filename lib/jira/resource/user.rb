cbf9cbae54c98a4d16958442c84a05be7ca8d016
Scott Ludtke <scott.ludtke@gmail.com>
2016-03-11 13:38:22 -0600

    added simple endpoint for the JIRA api myself endpoint. returning JSON from API at this point.

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -8,7 +8,12 @@ module JIRA
       def self.singular_path(client, key, prefix = '/')
         collection_path(client, prefix) + '?username=' + key
       end
+      
+      def myself        
+        myself_url = client.options[:rest_base_path] +  "/myself"
+        response = client.get(myself_url)
+        json = self.class.parse_json(response.body)
+      end
     end
-
   end
 end
