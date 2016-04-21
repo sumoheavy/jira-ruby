ab04a4ca0aefbb3704de436c2d41eadf3709de3e
Cameron Attard <cameron.attard@fairfaxmedia.com.au>
2016-04-21 16:42:45 +1000

    Implement UserFactory#myself

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -2,6 +2,12 @@ module JIRA
   module Resource
 
     class UserFactory < JIRA::BaseFactory # :nodoc:
+      def myself
+        instance = build
+        response = client.get("#{client.options[:rest_base_path]}/myself")
+        instance.set_attrs_from_response(response)
+        instance
+      end
     end
 
     class User < JIRA::Base
