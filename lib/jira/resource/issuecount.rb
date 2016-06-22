9ec218647837c646d40a3335ea7495e95c20b52f
Simon Guerrier <s.guerrier@criteo.com>
2016-06-22 14:12:57 +0200

    modified issue count

--- lib/jira/resource/issuecount.rb
+++ lib/jira/resource/issuecount.rb
@@ -16,12 +16,12 @@ module JIRA
         end
       end
 
-      def self.jql(client, jql, options = {fields: nil, start_at: nil, max_results: 0, expand: nil})
+      def self.jql(client, jql, options = {fields: nil, start_at: nil, expand: nil})
         url = client.options[:rest_base_path] + "/search?jql=" + CGI.escape(jql)
 
         url << "&fields=#{options[:fields].map{ |value| CGI.escape(value.to_s) }.join(',')}" if options[:fields]
         url << "&startAt=#{CGI.escape(options[:start_at].to_s)}" if options[:start_at]
-        url << "&maxResults=#{CGI.escape(options[:max_results].to_s)}" if options[:max_results]
+        url << "&maxResults=#{CGI.escape('0')}" if options[:max_results] # force maxResult to 0
 
         if options[:expand]
           options[:expand] = [options[:expand]] if options[:expand].is_a?(String)
