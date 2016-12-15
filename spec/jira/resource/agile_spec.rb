25b2e3dcca61fc8078e98f618f2cc11c83c303f0
peter.bell215 <peter.bell215@gmail.com>
2016-12-15 06:43:21 +0000

    Added tests for Agile.

---
+++ spec/jira/resource/agile_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe JIRA::Resource::Agile do
+  let(:client) { double(options: {rest_base_path: '/jira/rest/api/2', context_path: '/jira'}) }
+  let(:response) { double }
+
+  describe '#get_sprints' do
+    it 'should query correct url without parameters' do
+      expect(client).to receive(:get).with('/jira/rest/agile/1.0/board/1/sprint?maxResults=100').and_return(response)
+      expect(response).to receive(:body).and_return(get_mock_response('board/1.json'))
+
+      JIRA::Resource::Agile.get_sprints(client, 1)
+    end
+  end
+
+end
