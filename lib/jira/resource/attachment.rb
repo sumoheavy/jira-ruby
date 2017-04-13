62b3f95ef70750414e600fafbe8d9ede22894acf
Randy Carnahan <randy.carnahan@charter.com>
2017-04-13 16:00:32 -0400

    Added ability to upload attachments to an issue.
    Code is based on, but rewritten from:
    
    https://github.com/Hawatel/jira-ruby/blob/master/lib/jira/resource/attachment.rb
    
    Also removed unneeded .ruby-version file.

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -1,12 +1,43 @@
 module JIRA
   module Resource
-
     class AttachmentFactory < JIRA::BaseFactory # :nodoc:
     end
 
     class Attachment < JIRA::Base
-      has_one :author, :class => JIRA::Resource::User
-    end
+      belongs_to :issue
 
+      def self.endpoint_name
+        'attachments'
+      end
+
+      def save!(attrs)
+        path           = attrs['file_path']
+        name           = attrs['file_name'] || path
+        file           = File.new path
+        io             = UploadIO.new file, 'application/binary', name
+        data           = { 'file' => io }
+        headers        = { 'X-Atlassian-Token' => 'nocheck' }
+        request        = Net::HTTP::Post::Multipart.new url, data, headers
+        request_client = client.request_client
+        options        = client.request_client.options
+
+        request.basic_auth options[:username], options[:password]
+
+        response = request_client.basic_auth_http_conn.request request
+
+        set_attrs attrs, false
+
+        unless response.body.nil? || response.body.length < 2
+          json       = self.class.parse_json(response.body)
+          attachment = json[0]
+
+          set_attrs attachment
+        end
+
+        @expanded = false
+
+        true
+      end
+    end
   end
 end
