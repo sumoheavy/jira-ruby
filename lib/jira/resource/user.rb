c84f7feda61139a6b36c9e3efcd7b00aad91b778
Tomoya Kabe <limit.usus@gmail.com>
2018-05-28 23:48:23 +0900

    User search

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -8,11 +8,28 @@ module JIRA
         instance.set_attrs_from_response(response)
         instance
       end
+
+      delegate_to_target_class :search, :search_path
     end
 
     class User < JIRA::Base
       def self.singular_path(client, key, prefix = '/')
         collection_path(client, prefix) + '?username=' + key
+      end
+
+      def self.search_path(client, search, prefix='/')
+        collection_path(client, prefix) + '/search?username=' + search
+      end
+
+      def self.search(client, search, options = {})
+        response = client.get(search_path(client, search))
+        json = parse_json(response.body)
+        if collection_attributes_are_nested
+          json = json[endpoint_name.pluralize]
+        end
+        json.map do |attrs|
+          self.new(client, { attrs: attrs }.merge(options))
+        end
       end
     end
 
