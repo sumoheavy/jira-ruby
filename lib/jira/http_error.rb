58c2ecaba965f0d8ec71be679f836900b1cf65a9
A.B <a.b320012@gmail.com>
2018-08-16 10:15:27 -0400

    message to return response body

--- lib/jira/http_error.rb
+++ lib/jira/http_error.rb
@@ -4,11 +4,12 @@ module JIRA
   class HTTPError < StandardError
     extend Forwardable
 
-    def_instance_delegators :@response, :message, :code
-    attr_reader :response
+    def_instance_delegators :@response, :code
+    attr_reader :response, :message
 
     def initialize(response)
       @response = response
+      @message = response.try(:message) || response.try(:body)
     end
 
   end
