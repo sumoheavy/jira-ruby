9dbdb768299783ec161ae2f15b6713c622b12894
Rutger Gelling <rutger@hackerone.com>
2019-01-30 10:48:34 +0100

    Implement multipart requests for all client types

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -20,25 +20,28 @@ module JIRA
       end
 
       def save!(attrs)
-        headers = { 'X-Atlassian-Token' => 'nocheck' }
-        data = { 'file' => UploadIO.new(attrs['file'], 'application/binary', attrs['file']) }
+        headers = { 'X-Atlassian-Token' => 'no-check' }
+        data = { 'file' => UploadIO.new(attrs[:file], 'application/binary', attrs[:file]) }
 
-        request = Net::HTTP::Post::Multipart.new url, data, headers
-        request.basic_auth(client.request_client.options[:username],
-                           client.request_client.options[:password])
+        # Execute the multipart post here
+        response = client.post_multipart(url, data , headers)
 
-        response = client.request_client.basic_auth_http_conn.request(request)
-
-        set_attrs(attrs, false)
-        unless response.body.nil? || response.body.length < 2
-          json = self.class.parse_json(response.body)
-          attachment = json[0]
-
-          set_attrs(attachment)
-        end
+        set_attributes(attrs, response)
 
         @expanded = false
         true
+      end
+
+      private
+
+      def set_attributes(attributes, response)
+        set_attrs(attributes, false)
+        return if response.body.nil? || response.body.length < 2
+
+        json = self.class.parse_json(response.body)
+        attachment = json[0]
+
+        set_attrs(attachment)
       end
     end
   end
