79c71d87ea183192ad53ce084a36f6e7bf19d2b4
Alexei Emam <alexei@chaptermedia.com>
2019-07-04 14:33:11 +0100

    Update all-user-lookup for new JIRA-cloud conventions

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -10,15 +10,19 @@ module JIRA
     end
 
     class User < JIRA::Base
-      MAX_RESULTS = 1000
 
       def self.singular_path(client, key, prefix = '/')
         collection_path(client, prefix) + '?username=' + key
       end
 
+      def self.search_endpoint
+        'user/search'
+      end
       # Cannot retrieve more than 1,000 users through the api, please see: https://jira.atlassian.com/browse/JRASERVER-65089
-      def self.all(client)
-        response  = client.get("/rest/api/2/user/search?username=_&maxResults=#{MAX_RESULTS}")
+      def self.all(client, max_results: 1000)
+        search_url = [client.options[:rest_base_path], search_endpoint].join('/')
+        query_string = "username&maxResults=#{max_results}&includeInactive=true"
+        response  = client.get([search_url,query_string].join('?'))
         all_users = JSON.parse(response.body)
 
         all_users.flatten.uniq.map do |user|
