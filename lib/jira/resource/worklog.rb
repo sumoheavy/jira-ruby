5fa773ed631a468b149cd01c683288b53fce1f9e
Alexei Emam <alexei@chaptermedia.com>
2019-07-04 11:00:04 +0100

    Support worklog lookups by change/delete timestamps

--- lib/jira/resource/worklog.rb
+++ lib/jira/resource/worklog.rb
@@ -14,6 +14,71 @@ module JIRA
         'worklog'
       end
 
+      def self.find(client, ids, remote_limit: 1000)
+        return [] unless ids
+        ids = Array(ids)
+        worklogs_by_issue =
+          ids.each_slice(remote_limit).map do |the_ids|
+            response = client.post("#{client.options[:rest_base_path]}/#{endpoint_name}/list", JSON.dump({"ids"=>the_ids}))
+            json = parse_json(response.body)
+            json.map do |attrs|
+              self.new(
+                client,
+                {
+                  :attrs => attrs,
+                  :issue => JIRA::Resource::Issue.new(
+                    client,
+                    :attrs => {'id' => attrs['issueId']}
+                  )
+                }
+              )
+            end
+          end.flatten.group_by {|worklog| worklog.issue.id }
+
+          worklog_issue_ids = worklogs_by_issue.keys
+          issues =
+            worklog_issue_ids.each_slice(remote_limit).map do |the_issue_ids|
+              client.Issue.jql("id IN (#{the_issue_ids.join(',')})")
+            end.flatten
+
+          worklogs_by_issue.map do |issue_id, worklogs|
+            the_issue = issues.find {|issue| issue.id == issue_id }
+            worklogs.map do |worklog|
+              self.new(
+                client,
+                {
+                  :attrs => worklog.attrs,
+                  :issue => the_issue
+                }
+              )
+            end
+          end.flatten
+      end
+
+      def self.modified_after(client, timestamp)
+        response = client.get("#{client.options[:rest_base_path]}/#{endpoint_name}/updated?since=#{(timestamp.to_f * 1000).to_i}")
+        json = JSON.parse(response.body)
+        results = json['values']
+        while json['lastPage'] == false
+          response = client.get(json['nextPage'])
+          json = JSON.parse(response.body)
+          results += json['values']
+        end
+        find(client, results.map{|result| result['worklogId']})
+      end
+
+      def self.deleted_after(client, timestamp)
+        response = client.get("#{client.options[:rest_base_path]}/#{endpoint_name}/deleted?since=#{(timestamp.to_f * 1000)}")
+        json = JSON.parse response.body
+        results = json['values']
+        while json['lastPage'] == false
+          response = client.get(json['nextPage'])
+          json = JSON.parse(response.body)
+          results += json['values']
+        end
+        find(client, results.map{|result| result['worklogId']})
+      end
+
       def self.all(client, options = {})
         issue = options[:issue]
         raise ArgumentError, 'parent issue is required' unless issue
