e0c975936545b5aea2dc82c25464fcbed68be0f1
Prasad Anne <panne@zynga.com>
2020-02-28 07:19:20 -0500

    Favourite filters for the user
    Branch: "filters" | Commit Type: "New feature" | Changes tested: "yes"

--- lib/jira/resource/filter.rb
+++ lib/jira/resource/filter.rb
@@ -10,6 +10,15 @@ module JIRA
       def issues
         Issue.jql(client, jql)
       end
+
+      # Returns all the favourite filters for the user
+      def self.favourites(client, options = {})
+        search_url = client.options[:rest_base_path] + '/filter/favourite'
+        response = client.get(url_with_query_params(search_url, options))
+
+        json = parse_json(response.body)
+        client.Filter.build(json)
+      end
     end
   end
 end
