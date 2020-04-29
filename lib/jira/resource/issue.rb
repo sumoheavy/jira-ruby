53904c77f2b002e59db04511eca7af262b5b0243
David Hansen <david@hansen4.net>
2020-04-28 17:39:01 -0700

    Update Issue.jql options to be compliant with Jira V2 (though previously works under "Deprecated" support)

--- lib/jira/resource/issue.rb
+++ lib/jira/resource/issue.rb
@@ -66,13 +66,16 @@ module JIRA
         result
       end
 
-      def self.jql(client, jql, options = { fields: nil, start_at: nil, max_results: nil, expand: nil, validate_query: true })
+      def self.jql(client, jql, options = { fields: nil, start_at: nil, max_results: nil, expand: nil, validate_query: nil })
         url = client.options[:rest_base_path] + "/search?jql=#{CGI.escape(jql)}"
 
         url << "&fields=#{options[:fields].map { |value| CGI.escape(client.Field.name_to_id(value)) }.join(',')}" if options[:fields]
-        url << "&startAt=#{CGI.escape(options[:start_at].to_s)}" if options[:start_at]
-        url << "&maxResults=#{CGI.escape(options[:max_results].to_s)}" if options[:max_results]
-        url << '&validateQuery=false' if options[:validate_query] === false
+        url << "&startAt=#{CGI.escape(options[:start_at].to_s)}" unless options[:start_at].nil?
+        url << "&maxResults=#{CGI.escape(options[:max_results].to_s)}" unless options[:max_results].nil?
+
+        # Choices for validateQuery as of V2 are 'strict', 'warn', and 'none'
+        # (though 'true' and 'false' are still currently as "deprecated")
+        url << "&validateQuery=#{CGI.escape(options[:validate_query].to_s)}" unless options[:validate_query].nil?
 
         if options[:expand]
           options[:expand] = [options[:expand]] if options[:expand].is_a?(String)
