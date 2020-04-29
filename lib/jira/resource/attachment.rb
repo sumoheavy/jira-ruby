604688b5c4508845cd2e21bfa2f5ffec950cfa1c
David Hansen <david@hansen4.net>
2020-04-28 21:44:34 -0700

    Update attachment resource to separate out the filename from the file IO param

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -19,14 +19,28 @@ module JIRA
         parse_json(response.body)
       end
 
+      def self.all(client, options = {})
+        issue = options[:issue]
+        raise ArgumentError, 'parent issue is required' unless issue
+        attachments = issue.attachments
+        response = client.get(client.options[:rest_base_path] + '/attachment/')
+      end
+
+      def self.find(client, key, options = {})
+      end
+
       def save!(attrs, path = url)
         file = attrs['file'] || attrs[:file] # Keep supporting 'file' parameter as a string for backward compatibility
+        # If :filename does not exist or is nil, that is fine as it will force
+        # UpdateIO to determine the filename automatically from file.
+        # Breaking the filename out allows this to support any IO-based file parameter.
+        fname = attrs[:filename]
         mime_type = attrs[:mimeType] || 'application/binary'
 
         headers = { 'X-Atlassian-Token' => 'nocheck' }
-        data = { 'file' => UploadIO.new(file, mime_type, file) }
+        data = { 'file' => UploadIO.new(file, mime_type, fname) }
 
-        response = client.post_multipart(path, data , headers)
+        response = client.post_multipart(path, data, headers)
 
         set_attributes(attrs, response)
 
