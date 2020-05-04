e71efbfae721941b14c34bd5afe59af119957b40
Alexei Emam <alexei@chaptermedia.com>
2020-05-04 15:07:30 +0100

    Correct typo

--- lib/jira/base_factory.rb
+++ lib/jira/base_factory.rb
@@ -36,7 +36,7 @@ module JIRA
     # The principle purpose of this class is to delegate methods to the corresponding
     # non-factory class and automatically prepend the client argument to the argument
     # list.
-    delegate_to_target_class :all, :find, :collection_path, :singular_path, :jql, :get_backlog_issues, :get_board_issues, :get_sprints, :get_sprint_issues, :get_projects, :get_projects_full, , :revelio
+    delegate_to_target_class :all, :find, :collection_path, :singular_path, :jql, :get_backlog_issues, :get_board_issues, :get_sprints, :get_sprint_issues, :get_projects, :get_projects_full, :revelio
 
     # This method needs special handling as it has a default argument value
     def build(attrs = {})
