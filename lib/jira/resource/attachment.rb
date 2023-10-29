d5aacb0049beffcd68e1b3f252ee75ee5d6927db
Marlin Pierce <marlpier@cisco.com>
2023-10-29 11:46:31 -0400

    Attachment WIP

--- lib/jira/resource/attachment.rb
+++ lib/jira/resource/attachment.rb
@@ -9,6 +9,105 @@ module JIRA
       delegate_to_target_class :meta
     end
 
+
+    # This class provides the Attachment object <-> REST mapping for JIRA::Resource::Attachment derived class,
+    # i.e. the Create, Retrieve, Update, Delete lifecycle methods.
+    #
+    # == Lifecycle methods
+    #
+    # === Retrieving a single attachment by Issue and attachment id
+    #
+    #     issue =  JIRA::Resource::Issue.find(client, 'SUP-3000', { fields: 'attachment' } )
+    #     attachments = issue.attachments.find(30076)
+    #     attachment = attachments.first
+    #
+    # === Retrieving file contents of attachment
+    #
+    #     content = URI.open(attachment.content).read
+    #
+    # === Adding an attachment to an issue
+    #
+    #     client = JIRA::Client.new(options)
+    #
+    #     Tempfile.create('prefix_', Rails.configuration.cache_tmp, encoding: 'ascii-8bit') do |tempfile|
+    #       tempfile.write('data'.encode('binary'))
+    #       tempfile.rewind
+    #
+    #       issue =  JIRA::Resource::Issue.find(client, 'SUP-3000', { fields: 'attachment' } )
+    #       attachment = issue.attachments.build
+    #       attachment = JIRA::Resource::Attachment.new( client, issue: issue )
+    #       attachment.save!(file: tempfile.path, mimeType: 'application/octet-stream')
+    #     end
+    #
+    #       upload_file = UploadIO.new(tempfile, 'application/octet-stream', 'filename.dat')
+    #       upload_file = UploadIO.new(tempfile.path, 'application/octet-stream', 'filename.dat')
+    #     tempfile = Tempfile.new('prefix_')
+    #     tempfile.write(data.to_json)
+    #     tempfile.rewind
+    #     upload_file = UploadIO.new('/tmp/input', 'application/octet-stream', 'filename.dat')
+    # tempfile.path
+    #     upload_file = UploadIO.new('/tmp/input', 'application/json', 'filename.json')
+    #     upload_file = UploadIO.new('/tmp/input', 'application/octet-stream', 'filename.dat')
+    #     issue =  JIRA::Resource::Issue.find(client, 'SUP-3000', { fields: 'attachment' } )
+    #     attachment = issue.attachments.build
+    #     attachment.save(file: upload_file)
+    #
+    #     client.instance_variable_get("@options")[:default_headers].merge(headers)
+    #
+    # === Creating a resource
+    # TBD
+    #
+    #   resource = client.Resource.build({'name' => '')
+    #   resource.save
+    #
+    # === Updating a resource
+    # TBD
+    #
+    #   resource = client.Resource.find(id)
+    #   resource.save('updated_attribute' => 'new value')
+    #
+    # === Deleting a resource
+    # TBD
+    #
+    #   resource = client.Resource.find(id)
+    #   resource.delete
+    #
+    # == Nested resources
+    #
+    # Some resources are not defined in the top level of the URL namespace
+    # within the JIRA API, but are always nested under the context of another
+    # resource.  For example, a JIRA::Resource::Comment always belongs to a
+    # JIRA::Resource::Issue.
+    #
+    # These resources must be indexed and built from an instance of the class
+    # they are nested under:
+    #
+    #   issue = client.Issue.find(id)
+    #   comments = issue.comments
+    #   new_comment = issue.comments.build
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    # @!attribute [r] self
+    #   @return [String] URL to JSON of this attachment
+    # @!attribute [r] filename
+    #   @return [String] the filename
+    # @!attribute [r] author
+    #   @return [JIRA::Resource::User] the user who created the attachment
+    # @!attribute [r] created
+    #   @return [String] timestamp when the attachment was created
+    # @!attribute [r] size
+    #   @return [Integer] the file size
+    # @!attribute [r] mimeType
+    #   @return [String] MIME of the content type
+    # @!attribute [r] content
+    #   @return [String] URI to download the contents of the attachment
+    #
     class Attachment < JIRA::Base
       belongs_to :issue
       has_one :author, class: JIRA::Resource::User
