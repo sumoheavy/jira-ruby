1e8c8130b319e9c1f20de30bde519d89457c94c0
Kat Crichton-Seager <kat@clickmechanic.com>
2025-08-29 15:51:12 +0100

    fix: removes no-longer supported startAt parameter from .jql

--- lib/jira/resource/issue.rb
+++ lib/jira/resource/issue.rb
@@ -80,7 +80,7 @@ module JIRA
         result
       end
 
-      def self.jql(client, jql, options = { fields: nil, start_at: nil, max_results: nil, expand: nil, validate_query: true })
+      def self.jql(client, jql, options = { fields: nil, max_results: nil, expand: nil, validate_query: true })
         url = client.options[:rest_base_path] + "/search/jql?jql=#{CGI.escape(jql)}"
 
         if options[:fields]
@@ -88,7 +88,6 @@ module JIRA
                               CGI.escape(client.Field.name_to_id(value))
                             end.join(',')}"
         end
-        url << "&startAt=#{CGI.escape(options[:start_at].to_s)}" if options[:start_at]
         url << "&maxResults=#{CGI.escape(options[:max_results].to_s)}" if options[:max_results]
         url << '&validateQuery=false' if options[:validate_query] === false # rubocop:disable Style/CaseEquality
 
